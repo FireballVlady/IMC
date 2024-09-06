d5750e0d2718d2e22b42c0e510e03042c48c0fa7
meica 2.0 <meica 2.0@LAPTOP-6K01915R>
2024-09-06 13:48:44 -0600

    Agregar archivos de proyecto.

---
+++ IMC/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double peso;
+            double altura;
+            double masaCorporal;
+
+            Console.WriteLine("ingresa tu peso");
+            peso = int.Parse(Console.ReadLine());
+            Console.WriteLine("ingresa tu Altura");
+            altura = double.Parse(Console.ReadLine());
+
+            masaCorporal =  altura * altura;
+            masaCorporal = peso / masaCorporal;
+            Console.WriteLine("Este tu indice de masa corporal " + masaCorporal);
+
+            Console.ReadKey();
+        }
+    }
+}
